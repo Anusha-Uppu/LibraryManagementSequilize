dd653cb2347a0e89392307d6f3ad973550549509
Anusha-Uppu <anusha.uppu@everest.engineering>
2024-07-05 14:06:46 +0530

    assositions are added

---
+++ assosiations.js
@@ -0,0 +1,21 @@
+const author = require("./models/authors");
+const book = require("./models/books");
+const loan = require("./models/loans");
+const member = require("./models/members");
+const reservation = require("./models/reservations");
+
+book.belongsTo(author,{foreignKey:'authorId'});
+author.hasMany(book,{foreignKey:'authorId'});
+// member.belongsToMany(book,{through:loan});
+// Books and Loans
+book.hasMany(loan,{foreignKey:'book_id'});
+loan.belongsTo(book,{foreignKey:'book_id'});
+// Loans and Members
+member.hasMany(loan,{foreignKey:'member_id'})
+loan.belongsTo(member,{foreignKey:'member_id'})
+// Reservation and Members
+member.hasMany(reservation,{foreignKey:'member_id'});
+reservation.belongsTo(member,{foreignKey:'member_id'});
+// Books and Reservation
+book.hasMany(reservation,{foreignKey:'book_id'});
+reservation.belongsTo(book,{foreignKey:'book_id'});
